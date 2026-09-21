02c1a3b735897fc516a323d68dd67fcff5ba9c4d
Isabella Pardo <ipardot@eafit.edu.co>
2026-09-21 12:12:54 -0500

    Fix typo in button label and output text

--- app.py
+++ app.py
@@ -50,11 +50,11 @@ def text_to_speech(text, tld,lg):
 
 #display_output_text = st.checkbox("Verifica el texto")
 
-if st.button("convertir a Audio"):
+if st.button("Convertir a Audio"):
      result, output_text = text_to_speech(text, 'com',lg)#'tld
      audio_file = open(f"temp/{result}.mp3", "rb")
      audio_bytes = audio_file.read()
-     st.markdown(f"## Tú audio:")
+     st.markdown(f"## Tu audio:")
      st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
      #if display_output_text:
